8ddf74c64ffedf8907b95def483eb7ee361ac766
Bhavesh <bhaveshanjana58@gmail.com>
2026-03-17 08:27:32 +0530

    feat: update folder structure

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import express from "express";
-import connectTodb from "./db.js";
+import connectTodb from "./db/db.js";
 
 import invoiceRoute from "./routes/invoice.route.js";
 
